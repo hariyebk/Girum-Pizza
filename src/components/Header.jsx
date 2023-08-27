2a5c9945ec1224245b5317ec71012cf2846c700c
hariyebk <harunbekri6@gmail.com>
2023-08-27 11:43:16 +0300

    Small bug fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
         <header className="flex flex-wrap items-center justify-between gap-7 bg-yellow-400 border-b border-stone-200 uppercase px-4 py-3">
             <Link to = "/" className="tracking-widest"> Fast React Pizza co.</Link> 
             <Search/>
-            <Username />
+            <Username/>
         </header>
     )
 }
